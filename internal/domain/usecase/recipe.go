34b7b3d49ad9a703a4c22dc7270aa8bd0b3d76a5
willian <willian.cacoelho@gmail.com>
2025-02-07 15:19:17 -0300

    controller to get all recipes by id

--- internal/domain/usecase/recipe.go
+++ internal/domain/usecase/recipe.go
@@ -5,3 +5,7 @@ import "github.com/anuntech/finance-backend/internal/domain/models"
 type CreateRecipeRepository interface {
 	Create(recipe models.Recipe) (*models.Recipe, error)
 }
+
+type FindRecipesByWorkspaceIdRepository interface {
+	Find(workspaceId string) ([]models.Recipe, error)
+}
