6d1efc64eb1b1ec60815100285a8603e6739eb7c
willian <willian.cacoelho@gmail.com>
2025-02-10 12:45:00 -0300

    update cors

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 // Middleware para adicionar cabeçalhos CORS
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://anun.tech, http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "https://anun.tech")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
