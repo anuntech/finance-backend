d76eb3418d8ba96b84120352f9420782f81659de
willian <willian.cacoelho@gmail.com>
2025-04-17 16:19:04 -0300

    update bank cache

--- internal/setup/middlewares/allow_cache_header.go
+++ internal/setup/middlewares/allow_cache_header.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func AllowCacheHeader(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "public, max-age=3600")
+		w.Header().Set("Cache-Control", "public, max-age=60")
 		next.ServeHTTP(w, r)
 	})
 }
